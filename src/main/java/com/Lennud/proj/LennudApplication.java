7167653d31afe403b64d40b26ed457cffe57fafe
Madis-Julius <estonira@gmail.com>
2025-03-28 06:47:23 +0200

    first commit

---
+++ src/main/java/com/Lennud/proj/LennudApplication.java
@@ -0,0 +1,19 @@
+package com.Lennud.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LennudApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(LennudApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/Lennud/proj/LennudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lennud.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
