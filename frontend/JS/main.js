fc6058eb571fba6569910005af72312d54c08ae4
Madis-Julius <estonira@gmail.com>
2025-03-30 09:03:16 +0300

    Lennu andmete näitamine, lihtsad muudatused.

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -14,8 +14,44 @@ document.getElementById('fetchButton').addEventListener('click', async function(
         {
             const div = document.createElement('div');
             div.className = 'object';
-            div.textContent = `Hind: ${item.hind}`;
+
+            const flight = document.createElement('h2');
+            flight.className = 'flight';
+            flight.textContent = `${item.algkoht} -> ${item.sihtkoht}`;
+
+            const timediv = document.createElement('div');
+            timediv.className = 'time-container';
+            
+            const departure = document.createElement('h3');
+            departure.textContent = `Departure: ${item.lahkumisetund + ":" + item.lahkumiseminutid}`;
+            
+            const time = document.createElement('h3');
+            time.textContent = `Duration: ${item.lennuaeg} h`;
+            
+            const date = document.createElement('h3');
+            date.textContent = `Date: ${item.kuupäev}`;
+
+            const pricediv = document.createElement('div');
+            pricediv.className = 'price-container';
+
+            const select = document.createElement('button');
+            select.className = 'select-button';
+            select.textContent = 'Select';
+                        
+            const price = document.createElement('h2');
+            price.textContent = `Price: ${item.hind} €`;
+
+
             container.appendChild(div);
+            div.appendChild(flight);
+            div.appendChild(timediv)
+            timediv.appendChild(departure);
+            timediv.appendChild(time);
+            div.appendChild(date);
+            div.appendChild(pricediv);
+            pricediv.appendChild(select);
+            pricediv.appendChild(price);
+
         });
     } 
     catch (error) 
